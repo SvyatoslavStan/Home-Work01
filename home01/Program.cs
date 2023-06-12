7a857009f7c59f5c969ef98e5270f288a6d1a4ae
StanislavKhilyuk <geffie@yandex.ru>
2023-06-12 18:58:26 +0300

    added home01 home02 home03 home04 and gitignore

---
+++ home01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите 2 числа: ");
+
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = int.Parse(Console.ReadLine()!);
+
+int max = number1;
+
+if (number1 > number2) max = number1;
+if (number1 < number2) max = number2;
+
+Console.Write ("max = ");
+Console.WriteLine (max);
---
+++ home02/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("Введите 3 числа: ");
+
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = int.Parse(Console.ReadLine()!);
+int number3 = int.Parse(Console.ReadLine()!);
+
+
+int max = number1;
+
+if (number1 > max) max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+
+Console.Write ("max = ");
+Console.WriteLine (max);
